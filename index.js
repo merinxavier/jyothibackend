4ea168d7141dce60612c462e93d7367a6515c5c7
merinxavier <145212362+merinxavier@users.noreply.github.com>
2024-07-06 10:43:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-const express= require('express')
-const cors= require('cors');
-const app = express()
+const express = require('express');
+
+const cors = require('cors');
+
+
+const app = express();
+
 app.use(cors())
-app.get('/user',function(req,res){
+app.use(express.json());
+
+app.get('/user', function(req, res) {
     res.json('hello world')
 })
-app.post('/calculate',function (req,res){
-    let num1 =req.body.num1;
-    let num2 =req.body.num2;
-    let operator= req.body.operator;
-    let result=0;
+
+
+
+app.post('/calculate', function (req, res) {
+    let num1 = req.body.num1;
+    let num2 = req.body.num2;
+    let opporator = req.body.opporator;
+    let result;
+
+    if(opporator == 'add') {
+     result = num1 + num2;
+    } else if(opporator == 'sub') {
+        result = num1 - num2;
+    }else if(opporator == 'mul') {
+        result = num1 * num2;
+    } else if(opporator == 'div') {
+        result = num1 / num2;
+    }
+    res.json(result);
 });
-app.listen(2000,function(){
-    console.log('serverlistening on port 2000');
+app.listen(2000, function() {
+console.log('server listening on port 2000');
 })
